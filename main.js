fd49a7459ae71281546d331fc357f9181b741e94
Billy Bradley <billy@squeno.com>
2023-04-17 13:21:26 +0100

    Move description to info popup

--- main.js
+++ main.js
@@ -1,64 +1,82 @@
 function init() {
-    refreshFromStorage()
-    window.addEventListener('storage', e => { refreshFromStorage() })
+    document.addEventListener('DOMContentLoaded', () => {
+        refreshFromStorage()
+        window.addEventListener('storage', e => { refreshFromStorage() })
 
-    document.getElementById('message-input').addEventListener("keyup", e => {
-        if (e.key === "Enter") {
-            const messages = getMessages();
+        document.getElementById('message-input').addEventListener("keyup", e => {
+            if (e.key === "Enter") {
+                const messages = getMessages();
 
-            const author = getAuthor(messages);
+                const author = getAuthor(messages);
 
-            const messageInput = document.getElementById("message-input");
-            const text = messageInput.value;
-            messageInput.value = "";
-            const date = new Date();
+                const messageInput = document.getElementById("message-input");
+                const text = messageInput.value;
+                messageInput.value = "";
+                const date = new Date();
 
-            messages.push({
-                author,
-                text,
-                date,
-            });
+                messages.push({
+                    author,
+                    text,
+                    date,
+                });
 
-            localStorage.setItem('messages', JSON.stringify(messages));
-            window.dispatchEvent(new Event('storage'));
-        }
-    });
+                localStorage.setItem('messages', JSON.stringify(messages));
+                window.dispatchEvent(new Event('storage'));
+            }
+        });
 
-    const menuButton = document.getElementById('menu-button');
-    const menu = document.getElementById('menu');
-    menuButton.addEventListener('click', e => {
-        if (menu.style.display === 'none') {
-            menu.style.display = 'block';
-            menuButton.innerText = 'x';
-        } else {
-            menu.style.display = 'none';
-            menuButton.innerText = 'â˜°';
-        }
-    });
+        const menuButton = document.getElementById('menu-button');
+        const menu = document.getElementById('menu');
+        menuButton.addEventListener('click', e => {
+            if (menu.style.display === 'none') {
+                menu.style.display = 'block';
+                menuButton.innerText = 'x';
+            } else {
+                menu.style.display = 'none';
+                menuButton.innerText = 'â˜°';
+            }
+        });
 
-    document.getElementById('save-button').addEventListener('click', e => {
-        let a = document.createElement('a');
-        a.href = "data:application/octet-stream,"+encodeURIComponent(localStorage.getItem('messages'));
-        a.download = 'bossee-me.json';
-        a.click();
-    });
+        document.getElementById('save-button').addEventListener('click', e => {
+            let a = document.createElement('a');
+            a.href = "data:application/octet-stream," + encodeURIComponent(localStorage.getItem('messages'));
+            a.download = 'bossee-me.json';
+            a.click();
+        });
 
-    document.getElementById('load-button').addEventListener('change', e => {
-        var file = e.target.files[0];
+        document.getElementById('load-button').addEventListener('change', e => {
+            var file = e.target.files[0];
             if (!file) {
-            return;
-        }
-        
-        var reader = new FileReader();
-        
-        reader.onload = function(e) {
-            var contents = e.target.result;
-            localStorage.setItem('messages', contents);
-            window.dispatchEvent(new Event('storage'));
+                return;
+            }
+
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                var contents = e.target.result;
+                localStorage.setItem('messages', contents);
+                window.dispatchEvent(new Event('storage'));
+            };
+
+            reader.readAsText(file);
+        }, false);
+
+        const infoButton = document.getElementById('info-button');
+        const closeButton = document.getElementById('close-button');
+        const description = document.getElementById('description');
+
+        const toggleDescription = () => {
+            description.classList.toggle('show');
         };
-        
-        reader.readAsText(file);
-    }, false);
+
+        infoButton.addEventListener('click', toggleDescription);
+        closeButton.addEventListener('click', toggleDescription);
+        document.addEventListener('click', (e) => {
+            if (e.target !== infoButton && e.target !== closeButton && !description.contains(e.target) && description.classList.contains('show')) {
+                toggleDescription();
+            }
+        });
+    });
 }
 
 function refreshFromStorage() {
@@ -108,4 +126,4 @@ function scrollToBottom() {
     messages.scrollTop = messages.scrollHeight;
 }
 
-export default init ;
+export default init;
